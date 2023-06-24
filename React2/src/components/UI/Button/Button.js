d5bbcabc1f4d74b1baf093103958d2eb5084731a
신유수 <sin-yusu@sin-yusuui-MacBookPro.local>
2023-06-24 22:58:34 +0900

    initial and foldering

---
+++ React2/src/components/UI/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../../css/Button.css';
+
+const Button = props => {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
