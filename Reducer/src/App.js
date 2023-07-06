2664f882f5d3805ac3004ae5c02334d134e019ab
신유수 <sin-yusu@sin-yusuui-MacBookPro.local>
2023-07-06 18:53:06 +0900

    react useEffect

--- Reducer/src/App.js
+++ Reducer/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,29 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(()=>{
+    const checkUserLoggedin = localStorage.getItem('isLoggedIn');
+    
+    if (checkUserLoggedin === '1'){
+      setIsLoggedIn(true);
+    }
+  },[])
+  //useEffect는 2개의 인자를 받는데 앞에 인자는 익명 함수로 뒤에 인자는 의존성이고 조건에 맞을때만 한정적으로
+  //함수를 실행하고자 할때 쓴다 보통 사이드이펙트(http 데이터 fetch 등)를 다룰때 쓴다
+
+  //useEffect는 리엑트 앱의 렌더링이 끝나고 나서 실행 해서 로그인 체크처럼 앱이 재실행(보통 새로고침 등)될때
+  //렌더링을 중복하지 않도록 한다 여기선 2번째 인자로 빈배열을 주어서 앱이 첫 실행 될때만 실행하게 설정한다.
+  //만약 useEffect의 2번째 인자에 빈 배열도 안주면 컴포넌트가 state에 의해 변경될때마다 실행됨
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn','1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
--- Reducer/src/components/Login/Login.js
+++ Reducer/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from '../../css/Login.module.css';
@@ -11,12 +11,30 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      console.log(1)
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 2500);
+    return ()=>{
+      console.log(0)
+      clearTimeout(timer)
+      //clearTimeout을 설정함으로써 enteredEmail과 enteredPassword이 타이핑 될때 마다
+      //타임아웃 함수가 실행 하는데 만약 그 함수가 http request라면 프로그램 사용에 큰 오류를 줄 수 있기
+      //때문에 리턴으로 클리어 함수를 반환해야 입력이 완료 될때에만 함수를 보내도록 설정한다
+      
+      //만약 의존성이 빈 배열이고 useEffect의 클린업 함수가 있다면 컴포넌트가 dom에 제거될때 실행함
+    }
+  },[enteredEmail,enteredPassword])
+  //useEffect의 두번째 인자인 배열에 위 state를 주고 배열의 모든 요소가 전부 바뀌어야만 useEffect안의 함수
+  //를 실행하도록 해서 리엑트의 렌더링 횟수를 줄임
+  //하지만 이 함수는 onChange로 타이핑 하나하나 누를때마다 실행되기 때문에 사용자가 입력을 완료했다(2.5초)를
+  //지날때만 useEffect함수를 실행하게 함 
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
