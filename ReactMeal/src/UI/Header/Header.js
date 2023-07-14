e698dc10b3340948e1daa0cf057604234b833b70
신유수 <sin-yusu@sin-yusuui-MacBookPro.local>
2023-07-14 22:48:05 +0900

    modal

--- ReactMeal/src/UI/Header/Header.js
+++ ReactMeal/src/UI/Header/Header.js
@@ -1,20 +1,34 @@
-import classes from '../../Css/Header.module.css';
+import { useState } from "react";
+import classes from "../../Css/Header.module.css";
 
 import MealsSummary from "../MealsSummary";
-import HeaderCart from './HeaderCart';
+import HeaderCart from "./HeaderCart";
+import AddCartModal from "../../Modal/AddCartModal";
 
-const Header = props => {
-    return(
-        <>
-            <header className={classes.header}>ReactMeals
-                <HeaderCart/>
-            </header>
-                <div className={classes.mainImage}>
-                    <img alt='meals' src='img/meals.jpg'></img>
-                </div>
-            <MealsSummary/>
-        </>
-    )
+const Header = (props) => {
+  const [modal, setModal] = useState(false);
+
+  const showModal = () => {
+    setModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setModal(false);
+  };
+
+  return (
+    <>
+      {modal && <AddCartModal closeModal={handleCloseModal} />}
+      <header className={classes.header}>
+        ReactMeals
+        <HeaderCart getClick={showModal} />
+      </header>
+      <div className={classes.mainImage}>
+        <img alt="meals" src="img/meals.jpg"></img>
+      </div>
+      <MealsSummary />
+    </>
+  );
 };
 
 export default Header;
