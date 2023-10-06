0151c58432bf6328ddc31b81243b947d19bb2fc5
sin-yusu@sinyusucBookPro <tlsdbtn0507@naver.com>
2023-10-06 17:56:24 +0900

    css foldering

--- Context/src/components/Products/ProductItem.js
+++ Context/src/components/Products/ProductItem.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { useDispatch } from "react-redux";
 
-import Card from '../UI/Card';
-import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
+import Card from "../UI/Card";
+import "../../css/ProductItem.css";
+import { toggleFav } from "../../store/actions/products";
 
-const ProductItem = props => {
+const ProductItem = (props) => {
   const dispatch = useDispatch();
 
   const toggleFavHandler = () => {
@@ -13,15 +13,15 @@ const ProductItem = props => {
   };
 
   return (
-    <Card style={{ marginBottom: '1rem' }}>
+    <Card style={{ marginBottom: "1rem" }}>
       <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
         <p>{props.description}</p>
         <button
-          className={!props.isFav ? 'button-outline' : ''}
+          className={!props.isFav ? "button-outline" : ""}
           onClick={toggleFavHandler}
         >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
+          {props.isFav ? "Un-Favorite" : "Favorite"}
         </button>
       </div>
     </Card>
