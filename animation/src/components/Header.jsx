09678c54d24a7ed08fab6973edf22e4a5bc66c3f
sin-yusu@sinyusucBookPro <tlsdbtn0507@naver.com>
2023-10-07 21:39:35 +0900

    framer를 이용해서 css변환의 편의(회전, 모달 삭제)를 줌

--- animation/src/components/Header.jsx
+++ animation/src/components/Header.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 
-import NewChallenge from './NewChallenge.jsx';
+import NewChallenge from "./NewChallenge.jsx";
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -15,7 +16,11 @@ export default function Header() {
 
   return (
     <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      <AnimatePresence>
+        {/* React는 dom요소를 즉시 삭제시키기 때문에 AnimatePresence를 써야 dom에서 삭제되기 전에
+      원하는 animation을 적용시킬 수 있음 */}
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
 
       <header id="main-header">
         <h1>Your Challenges</h1>
