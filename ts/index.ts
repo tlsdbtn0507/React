67e4160f87a93a8e084ba34cee55f24f1f190d81
yusuShin <tlsdbtn0507@naver.com>
2023-12-13 21:05:28 +0900

    start ts

---
+++ ts/index.ts
@@ -0,0 +1,5 @@
+function add (a:number,b:number){
+  return a+b
+}
+const result = add(1,2)
+console.log(result)
---
+++ ts/index1.js
@@ -0,0 +1,4 @@
+var add2 = function (a, b) {
+    return a + b;
+};
+console.log(add(1, 2));
